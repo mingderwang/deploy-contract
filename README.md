73e2e4cf5a799d8d6e54ebbd7e636f5be5513217
Wang Ming-der <ming@log4analytics.com>
2017-03-05 16:41:13 +0800

    ok for deploy the contract - BasicToken.sol

---
+++ README.md
@@ -0,0 +1,1 @@
+test code for https://medium.com/taipei-ethereum-meetup/使用node-js部署智能合約-smart-contract-520534305aaf
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const solc = require('solc');
 * connect to ethereum node
 */ 
 const ethereumUri = 'http://192.168.1.106:8540';
-const address = '0x007802C7D8247c09a459030830223B61a7C90Ba1';
+const address = '0x00d57fA3AcB844E0fD773B3a1Ae70AD946069Bf8';
 
 let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(ethereumUri));
@@ -42,4 +42,46 @@ let coinbase = web3.eth.coinbase;
     }
 
 console.log(JSON.stringify(abi, undefined, 2));
+
+
+/*
+* deploy contract
+*/ 
+let gasEstimate = web3.eth.estimateGas({data: '0x' + bytecode});
+console.log('gasEstimate = ' + gasEstimate);
+
+let MyContract = web3.eth.contract(abi);
+console.log('deploying contract...');
+
+let myContractReturned = MyContract.new([], {
+    from: address,
+    data: '0x'+ bytecode,
+    gas: gasEstimate + 593556
+}, function (err, myContract) {
+    if (!err) {
+        // NOTE: The callback will fire twice!
+        // Once the contract has the transactionHash property set and once its deployed on an address.
+      
+        // e.g. check tx hash on the first call (transaction send)
+        if (!myContract.address) {
+            console.log(`myContract.transactionHash = ${myContract.transactionHash}`); // The hash of the transaction, which deploys the contract
+          
+        // check address on the second call (contract deployed)
+        } else {
+            console.log(`myContract.address = ${myContract.address}`); // the contract address
+            global.contractAddress = myContract.address;
+        }
+      
+        // Note that the returned "myContractReturned" === "myContract",
+        // so the returned "myContractReturned" object will also get the address set.
+    } else {
+        console.log(err);
+    }
+});
+
+(function wait () {
+    setTimeout(wait, 1000);
+})();
 }
+
+
